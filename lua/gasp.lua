24bc8df807d4980f15b05a754a257832820a2e89
awsdert <gb2985@gmail.com>
2020-02-04 22:25:12 +0000

    Remove all the makefile stuff gasp no longer needs as it was only introduced while I was trying to figure out how to get GUI mode running under pkexec, also start making the font selection look more appropriate for future usage, for time being I will be building the basics of the main GUI for which to find a process and hook it, following that I will build the scanning GUI before finally working on record GUI (Similar concept to Cheat Engine there for sake of lowering the learning curve)

--- lua/gasp.lua
+++ lua/gasp.lua
@@ -1,94 +1,98 @@
-#!/usr/bin/env lua
+-- #!/usr/bin/env lua
 -- Simple example that shows how to change font when drawing widgets.
 -- Uses the GLFW/OpenGL rear shipped with MoonNuklear.
 
-local glfw = require("moonglfw")
-local gl = require("moongl")
-local rear = require("moonnuklear.glbackend")
-local nk = require("moonnuklear")
+gl = require("moongl")
+glfw = require("moonglfw")
+nk = require("moonnuklear")
+rear = require("moonnuklear.glbackend")
 
-local TITLE = "Changing font" -- GLFW window title
-local FPS = 30 -- desired frames per seconds
-local W, H = 800, 640 -- window width and height
-local BGCOLOR = {0.10, 0.18, 0.24, 1.0} -- background color
-local VBO_SIZE, EBO_SIZE = 512*1024, 128*1024 -- size of GL buffers
-local ANTI_ALIASING = true -- use anti-aliasing
-local CLIPBOARD = false -- use clipboard
-local ROOT_FONT_DIR = '/usr/share/fonts'
-local DROID_FONT_DIR = ROOT_FONT_DIR .. '/droid'
-local NOTO_FONT_DIR = ROOT_FONT_DIR .. '/noto'
-local GASP_FONT_DIR = ROOT_FONT_DIR .. '/TTF'
-local GASP_FONT_1EM = 20
-local GASP_FONT_USE = 'default'
+gui = {}
+cfg = {
+	fps = 30,
+	clipboard = false,
+	anti_aliasing = false,
+	window = {
+		width = 1600,
+		height = 900,
+		title = "GASP - Gaming Assistive tech for Solo Play"
+	},
+	colors = {
+		-- RGBA from 0.0 to 1.0
+		bg = { 0.1, 0.1, 0.1, 1.0 }
+	},
+	sizes = {
+		gl_vbo = 1024 * 512,
+		gl_ebo = 1024 * 128
+	},
+	dir = {
+		sys = "/usr/share",
+		usr = '~/.config/.gasp',
+		otg = "./otg"
+	}
+}
+cfg.font = {
+	-- *.ttf fonts only should be used here unless nuklear gets an upgrade
+	file = cfg.dir.sys .. "/fonts/noto/NotoSansDisplay-Regular.ttf",
+	base_size = 40,
+	use = "medium"
+}
 
 local R, G, B, A
-local window, ctx, atlas
+local atlas
 
 local function set_bgcolor(color)
-   BGCOLOR = color or BGCOLOR
-   R, G, B, A = table.unpack(BGCOLOR)
+   cfg.colors.bg = color or cfg.colors.bg
+   return table.unpack(cfg.colors.bg)
 end
-set_bgcolor()
+R, G, B, A = set_bgcolor()
 
 -- GL/GLFW inits
 glfw.version_hint(3, 3, 'core')
-window = glfw.create_window(W, H, TITLE)
-glfw.make_context_current(window)
+gui.window = glfw.create_window(
+	cfg.window.width, cfg.window.height, cfg.window.title )
+glfw.make_context_current(gui.window)
 gl.init()
 
 -- Initialize the rear
-ctx = rear.init(window, {
-   vbo_size = VBO_SIZE,
-   ebo_size = EBO_SIZE,
-   anti_aliasing = ANTI_ALIASING,
-   clipboard = CLIPBOARD,
+gui.ctx = rear.init(gui.window, {
+   vbo_size = cfg.sizes.gl_vbo,
+   ebo_size = cfg.sizes.gl_ebo,
+   anti_aliasing = cfg.anti_aliasing,
+   clipboard = cfg.clipboard,
    callbacks = true
 })
 
 -- Load fonts: if none of these are loaded a default font will be used
 atlas = rear.font_stash_begin()
-function gasp_add_font(name,size,dir)
-	if not dir then
-		dir = GASP_FONT_DIR
+function gasp_add_font(size)
+	if type(size) == "number" then
+		size = cfg.font.base_size * size
 	else
-		dir = ROOT_FONT_DIR .. '/' .. dir
+		size = cfg.font.base_size
 	end
-	if size == "large" then
-		size = GASP_FONT_1EM + 4
-	elseif size == "small" then
-		size = GASP_FONT_1EM - 4
-	else
-		size = GASP_FONT_1EM
-	end
-	if not name then
-		name = dir .. "/"
-	else
-		name = dir .. "/" .. name
-	end
-	print( "Adding font '" .. name .. "' at size " .. size )
-	return atlas:add( size, name )
+	return atlas:add( size, cfg.font.file )
 end
-local fonts = {
-	default = gasp_add_font( "DejaVuSans.ttf" ),
-	droid = gasp_add_font( "DroidSans.ttf",nil,"droid" ),
-	droid_large = gasp_add_font( "DroidSans.ttf", "large","droid" ),
-	noto = gasp_add_font( "NotoSansDisplay-Regular.ttf", nil, "noto" ),
-	noto_large = gasp_add_font( "NotoSansDisplay-Regular.ttf", "large", "noto" ),
-	noto_small = gasp_add_font( "NotoSansDisplay-Regular.ttf", "small", "noto" )
-	--[[
-	cjk = gasp_add_font( "NotoSansCJK-Regular.ttc", nil, "noto-cjk" ),
-	cjk_large = gasp_add_font( "NotoSansCJK-Regular.ttc", "large", "noto-cjk" ),
-	cjk_small = gasp_add_font( "NotoSansCJK-Regular.ttc", "small", "noto-cjk" )
-	]]
-}
+gui.font_size_mul = {}
+gui.font_size_mul["xx-large"] = 1.6
+gui.font_size_mul["x-large"] = 1.4
+gui.font_size_mul["large"] = 1.2
+gui.font_size_mul["medium"] = 1.0
+gui.font_size_mul["small"] = 0.8
+gui.font_size_mul["x-small"] = 0.6
+gui.font_size_mul["xx-small"] = 0.4
+gui.fonts = {}
+gui.fonts["xx-large"] = gasp_add_font(gui.font_size_mul["xx-large"])
+gui.fonts["x-large"] = gasp_add_font(gui.font_size_mul["x-large"])
+gui.fonts["large"] = gasp_add_font(gui.font_size_mul["xx-large"])
+gui.fonts["medium"] = gasp_add_font(gui.font_size_mul["medium"])
+gui.fonts["small"] = gasp_add_font(gui.font_size_mul["small"])
+gui.fonts["x-small"] = gasp_add_font(gui.font_size_mul["x-small"])
+gui.fonts["xx-small"] = gasp_add_font(gui.font_size_mul["xx-small"])
 gasp_add_font = nil
-rear.font_stash_end( ctx, fonts.default )
+rear.font_stash_end( gui.ctx, gui.fonts.medium )
 
--- Set the default font to 'droid':
---nk.style_set_font(ctx, fonts.droid)
-
-print("Setting callback")
-glfw.set_key_callback(window,
+glfw.set_key_callback(gui.window,
 function (window, key, scancode, action, shift, control, alt, super)
    if key == 'escape' and action == 'press' then
       glfw.set_window_should_close(window, true)
@@ -99,48 +103,91 @@ end)
 collectgarbage()
 collectgarbage('stop')
 
-local window_flags = nk.WINDOW_BORDER |	nk.WINDOW_MOVABLE | nk.WINDOW_CLOSABLE | nk.WINDOW_SCALABLE
+gui.window_flags = nk.WINDOW_BORDER | nk.WINDOW_SCALABLE
 
-function draw()
-	local used_default = (GASP_FONT_USE == 'default')
-	if used_default == false then
-		nk.style_push_font(ctx, fonts[GASP_FONT_USE])
-	end
-	-- Draw the GUI --------------------------------------------
-	if nk.window_begin(ctx, "GUI", {50, 50, 320, 220}, window_flags) then
-		nk.layout_row_dynamic(ctx, 20, 1)
-		if nk.button(ctx, nil, "Default Font") then
-			 GASP_FONT_USE = 'noto'
-		end
-		if nk.button(ctx, nil, "Noto Font") then
-			 GASP_FONT_USE = 'noto'
-		end
-		if nk.button(ctx, nil, "Noto Large Font") then
-			 GASP_FONT_USE = 'noto_small'
-		end
-		if nk.button(ctx, nil, "Noto Small Font") then
-			 GASP_FONT_USE = 'noto_small'
-		end
-	end
-	if used_default == false then
-		nk.style_pop_font(ctx)
-	end
-	nk.window_end(ctx)
-	------------------------------------------------------------
+local function pad_width(font,text)
+	return math.ceil((font:width(font:height(),text)) * 1.2)
+end
 
+local function pad_height(font,text)
+	return math.ceil((font:height(text)) * 1.2)
+end
+
+function draw_font_sizes_window()
+	local normal_font = (cfg.font.use == 'medium')
+	local bounds = {0,0,cfg.window.width,cfg.window.height}
+	
+	local add_label = function(name,text)
+		local used = (cfg.font.use == name)
+		local font = gui.fonts[name]
+		if used == false then
+			nk.style_push_font(gui.ctx, font)
+		end
+		nk.layout_row_static(gui.ctx,
+			pad_height(font,text), pad_width(font,text), #text )
+		nk.label(gui.ctx,text,nk.WINDOW_BORDER)
+		if used == false then
+			nk.style_pop_font(gui.ctx)
+		end
+	end
+	
+	local add_button = function(name,text)
+		local used = (cfg.font.use == name)
+		local font = gui.fonts[name]
+		if used == false then
+			nk.style_push_font(gui.ctx, font)
+		end
+		nk.layout_row_static(gui.ctx,
+			pad_height(font,text), pad_width(font,text), #text )
+		if nk.button(gui.ctx, nil, text) then
+			 cfg.font.use = name
+		end
+		if used == false then
+			nk.style_pop_font(gui.ctx)
+		end
+	end
+	
+	if normal_font == false then
+		nk.style_push_font(gui.ctx, gui.fonts[cfg.font.use])
+	end
+	
+	if nk.window_begin(gui.ctx, "Font Size",
+		bounds, gui.window_flags) then
+		
+		add_label(cfg.font.use,"Change font size to:")
+		
+		add_button( 'xx-large', 'XX-Large')
+		add_button( 'x-large', 'X-Large')
+		add_button( 'large', 'Large')
+		add_button( 'medium', 'Medium')
+		add_button( 'x-small', 'Small')
+		add_button( 'x-small', 'X-Small')
+		add_button( 'xx-small', 'XX-Small')
+	end
+	
+	if normal_font == false then
+		nk.style_pop_font(gui.ctx)
+	end
+	nk.window_end(gui.ctx)
+end
+function draw_all()
+	cfg.window.width, cfg.window.height =
+		glfw.get_window_size(gui.window)
+	draw_font_sizes_window()
 	-- Render
-	W, H = glfw.get_window_size(window)
-	gl.viewport(0, 0, W, H)
+	cfg.window.width, cfg.window.height =
+		glfw.get_window_size(gui.window)
+	gl.viewport(0, 0, cfg.window.width, cfg.window.height)
 	gl.clear_color(R, G, B, A)
 	gl.clear('color')
 	rear.render()
-	glfw.swap_buffers(window)
+	glfw.swap_buffers(gui.window)
 	collectgarbage()
 end
-while not glfw.window_should_close(window) do
-	glfw.wait_events_timeout(1/FPS) --glfw.poll_events()
+while not glfw.window_should_close(gui.window) do
+	glfw.wait_events_timeout(1/cfg.fps)
 	rear.new_frame()
-	draw()
+	draw_all()
 end
 rear.shutdown()
 --[[
--- makefile
+++ makefile
@@ -38,13 +38,17 @@ debugger_gasp_exe:=debugger_$(gasp_dbg_exe)
 private_gasp_exe:=private_$(gasp_exe)
 private_gasp_dbg_exe:=private_$(gasp_dbg_exe)
 
-.PHONY: default all gasp debug makefile clean
-default: gasp
+.PHONY: default all run gede gasp debug makefile clean
+default: run
 
 lua_dir=cloned/lua
 moongl_dir=cloned/moongl
 moonglfw_dir=cloned/moonglfw
 moonnuklear_dir=cloned/moonnuklear
+lua_src_dir:=$(lua_dir)
+moongl_src_dir:=$(moongl_dir)/src
+moonglfw_src_dir:=$(moonglfw_dir)/src
+moonnuklear_src_dir:=$(moonnuklear_dir)/src
 
 $(call mkdir,cloned)
 $(call clone,$(lua_dir),cloned,lua/lua)
@@ -56,34 +60,10 @@ $(call pull,$(moongl_dir))
 $(call pull,$(moonglfw_dir))
 $(call pull,$(moonnuklear_dir))
 
-lua_src_dir=$(lua_dir)
-lua_sources:=$(filter-out $(lua_src_dir)/lua.c $(lua_src_dir)/onelua.c,$(wildcard $(lua_src_dir)/*.c))
-lua_objects:=$(lua_sources:%=%.o)
-lua_dbgobjs:=$(lua_sources:%=%-d.o)
-lua_dll:=liblua.$(dll_ext)
-lua_dbg:=liblua-d.$(dll_ext)
-
-moongl_src_dir:=$(moongl_dir)/src
-moongl_sources:=$(wildcard $(moongl_src_dir)/*.c)
-moongl_objects:=$(moongl_sources:%=%.o)
-moongl_dbgobjs:=$(moongl_sources:%=%-d.o)
-moongl_dll:=libmoongl.$(dll_ext)
-moongl_dbg:=libmoongl-d.$(dll_ext)
-moonglfw_src_dir:=$(moonglfw_dir)/src
-moonglfw_sources:=$(wildcard $(moonglfw_src_dir)/*.c)
-moonglfw_objects:=$(moonglfw_sources:%=%.o)
-moonglfw_dbgobjs:=$(moonglfw_sources:%=%-d.o)
-moonglfw_dll:=libmoonglfw.$(dll_ext)
-moonglfw_dbg:=libmoonglfw-d.$(dll_ext)
-moonnuklear_src_dir:=$(moonnuklear_dir)/src
-moonnuklear_sources:=$(wildcard $(moonnuklear_src_dir)/*.c)
-moonnuklear_objects:=$(moonnuklear_sources:%=%.o)
-moonnuklear_dbgobjs:=$(moonnuklear_sources:%=%-d.o)
-moonnuklear_dll:=libmoonnuklear.$(dll_ext)
-moonnuklear_dbg:=libmoonnuklear-d.$(dll_ext)
+gasp_args=-D HELLO="WORLD"
 bin_flags=-fPIC
 rpath=-L'.' -Wl,-rpath,'.'
-exe_flags=$(bin_flags) -ldl -lm -lpthread
+exe_flags=$(bin_flags) -ldl -lm -lpthread -llua
 lib_flags=$(exe_flags) -shared
 inc_flags=-I $(lua_dir) -I $(moonnuklear_dir) -I $(moonglfw_dir) -I $(moongl_dir)
 win32_defines=_WIN32
@@ -92,7 +72,13 @@ defines:=$(if $(IS_LINUX),$(linux_defines),$(win32_defines))
 src_flags=$(bin_flags) -Wall -std=c99 $(defines:%=-D %) -DLUAVER=5.3
 dbg_flags=-ggdb -D _DEBUG
 
-moon_libs_original:
+libs: lua_lib moon_libs
+
+lua_lib:
+	cd $(lua_dir) && make
+	cp $(lua_src_dir)liblua.so liblua.so
+
+moon_libs:
 	cd $(moongl_dir) && make
 	cd $(moonglfw_dir) && make
 	cd $(moonnuklear_dir) && make
@@ -113,62 +99,37 @@ clean:
 	rm -f $(moonglfw_src_dir)/*.so
 	rm -f $(moonnuklear_src_dir)/*.so
 
-leaks: $(gasp_dbg_exe)
-	valgrind --leak-check=full --show-leak-kinds=all ./$(gasp_dbg_exe)
+leaks: gasp debug
+	valgrind -s ./$(private_gasp_dbg_exe) $(gasp_args)
+	valgrind -s ./$(debugger_gasp_exe) $(gasp_args)
+	valgrind -s ./$(gasp_dbg_exe) $(gasp_args)
+	valgrind -s ./$(private_gasp_exe) $(gasp_args)
+	valgrind -s ./$(gasp_exe) $(gasp_args)
+
+run: gasp
+	./$(gasp_exe) $(gasp_args)
+
+gede: debug
+	gede --args ./$(gasp_dbg_exe) $(gasp_args)
 
 gasp: $(gasp_exe) $(private_gasp_exe)
-	./$(gasp_exe) -D HELLO="WORLD"
-
-$(gasp_exe): $(exposed_gasp_objects) $(moonnuklear_dll)
-	$(CC) $(exe_flags) -llua -o $@ $(exposed_gasp_objects)
-
-$(private_gasp_exe): $(private_gasp_objects) $(moonnuklear_dll)
-	$(CC) $(exe_flags) -llua -o $@ $(private_gasp_objects)
 
 debug: $(gasp_dbg_exe) $(private_gasp_dbg_exe) $(debugger_gasp_exe)
-	gede --args ./$(gasp_dbg_exe) -D HELLO="WORLD"
 
-valgrind: $(gasp_dbg_exe) $(private_gasp_dbg_exe) $(debugger_gasp_exe)
-	#valgrind -s ./$(gasp_dbg_exe) -D HELLO="WORLD"
-	valgrind -s ./$(debugger_gasp_exe) -D HELLO="WORLD"
-	#valgrind -s ./$(private_gasp_dbg_exe) -D HELLO="WORLD"
+$(gasp_exe): $(exposed_gasp_objects)
+	$(CC) $(exe_flags) -o $@ $(exposed_gasp_objects)
 
-$(gasp_dbg_exe): $(exposed_gasp_dbgobjs) $(moonnuklear_dbg)
-	$(CC) $(dbg_flags) $(exe_flags) -llua -o $@ $(exposed_gasp_dbgobjs)
+$(private_gasp_exe): $(private_gasp_objects)
+	$(CC) $(exe_flags) -o $@ $(private_gasp_objects)
 
-$(private_gasp_dbg_exe): $(private_gasp_dbgobjs) $(moonnuklear_dbg)
-	$(CC) $(dbg_flags) $(exe_flags) -llua -o $@ $(private_gasp_dbgobjs)
+$(gasp_dbg_exe): $(exposed_gasp_dbgobjs)
+	$(CC) $(dbg_flags) $(exe_flags) -o $@ $(exposed_gasp_dbgobjs)
 
-$(debugger_gasp_exe): $(debugger_gasp_dbgobjs) $(moonnuklear_dbg)
-	$(CC) $(dbg_flags) $(exe_flags) -llua -o $@ $(debugger_gasp_dbgobjs)
+$(private_gasp_dbg_exe): $(private_gasp_dbgobjs)
+	$(CC) $(dbg_flags) $(exe_flags) -o $@ $(private_gasp_dbgobjs)
 
-$(moonnuklear_dll): $(moonnuklear_objects) $(moonglfw_dll)
-	$(CC) $(lib_flags) -llua -o $@ $(moonnuklear_objects)
-
-$(moonnuklear_dbg): $(moonnuklear_dbgobjs) $(moonglfw_dbg)
-	$(CC) $(dbg_flags) $(lib_flags) -llua -o $@ $(moonnuklear_dbgobjs)
-
-$(moonglfw_dll): $(moonglfw_objects) $(moongl_dll)
-	$(CC) $(lib_flags) -llua -o $@ $(moonglfw_objects)
-
-$(moonglfw_dbg): $(moonglfw_dbgobjs) $(moongl_dbg)
-	$(CC) $(dbg_flags) $(lib_flags) -llua -o $@ $(moonglfw_dbgobjs)
-
-$(moongl_dll): $(moongl_objects) $(lua_dll)
-	$(CC) $(lib_flags) -llua -o $@ $(moongl_objects)
-
-$(moongl_dbg): $(moongl_dbgobjs) $(lua_dbg)
-	$(CC) $(dbg_flags) $(lib_flags) -llua -o $@ $(moongl_dbgobjs)
-
-$(lua_dll): $(lua_objects)
-	$(CC) $(lib_flags) -o $@ $^
-
-$(lua_dbg): $(lua_dbgobjs)
-	$(CC) $(dbg_flags) $(lib_flags) -o $@ $^
-
-$(moongl_src_dir)/%.o: $(wildcard $(moongl_src_dir)/*.h)
-$(moonglfw_src_dir)/%.o: $(wildcard $(moonglfw_src_dir)/*.h)
-$(moonnuklear_src_dir)/%.o: $(wildcard $(moonnuklear_src_dir)/*.h)
+$(debugger_gasp_exe): $(debugger_gasp_dbgobjs)
+	$(CC) $(dbg_flags) $(exe_flags) -o $@ $(debugger_gasp_dbgobjs)
 
 %.o: %
 	$(CC) $(flags) $(inc_flags) $(src_flags) -o $@ -c $<
